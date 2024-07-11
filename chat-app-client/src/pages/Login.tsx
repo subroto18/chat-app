8884f4c8f063b4b7e87cf3e2d6b2303af34bb7ba
subroto18 <subroto@revivotech.in>
2024-07-11 09:15:14 +0530

    command to check render deployment

--- chat-app-client/src/pages/Login.tsx
+++ chat-app-client/src/pages/Login.tsx
@@ -1,10 +1,8 @@
-import React from "react";
-
 import Login from "../components/Auth";
 const Home = () => {
   return (
     <div className="bg-[url('https://www.shutterstock.com/image-vector/social-media-sketch-vector-seamless-600nw-1660950727.jpg')] h-[100vh] w-[100vw] flex justify-center items-center">
-      <Login />
+      {/* <Login /> */}
     </div>
   );
 };
