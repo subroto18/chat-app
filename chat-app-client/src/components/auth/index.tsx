882b5561f61e7b029de5a2fa98da82ebc21c8ad3
subroto18 <subroto@revivotech.in>
2024-07-14 10:58:22 +0530

    add logo

--- chat-app-client/src/components/auth/index.tsx
+++ chat-app-client/src/components/auth/index.tsx
@@ -3,8 +3,8 @@ import Tab from "./Tab";
 const index = () => {
   return (
     <div className=" w-[25rem] h-auto">
-      <div className="text-center py-5 rounded-sm shadow-md border-spacing-1 bg-slate-100">
-        <h1>Talk-A-Tive</h1>
+      <div className="flex justify-center py-5 rounded-sm shadow-md border-spacing-1 bg-slate-100">
+        <img src="/logo.png" className="h-[80px]" />
       </div>
       <div className="bg-slate-100">
         <Tab />
